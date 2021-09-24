70a723bcaa3201315e0acc72632c43dc567aee2a
= <rtyenriques@gmail.com>
2021-09-24 16:02:29 -0700

    commit tree d09d57eb543c3f21ea06a77870f016c88cab7182

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,16 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :post_tags
   has_many :tags, :through => :post_tags
+  accepts_nested_attributes_for :tags, reject_if: proc {|attributes| attributes['name'].blank?}
 
   validates_presence_of :name, :content
+
+
+#   def tags_attributes=(tag)
+#    tag.each do |t|
+#     ta = Tag.find_or_create_by(t)
+#     self.tags << ta
+#    end
+
+#   end
 end
